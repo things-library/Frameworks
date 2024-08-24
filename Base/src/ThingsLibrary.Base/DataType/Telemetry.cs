c714b04136e7cbbab24b43d269be8ae5e0ab25ee
Mark Lanning <Mark.Lanning@landus.ag>
2024-08-24 12:16:27 -0500

    updating telemetry data type

---
+++ Base/src/ThingsLibrary.Base/DataType/Telemetry.cs
@@ -0,0 +1,52 @@
+﻿using ThingsLibrary.Schema.Library.Extensions;
+
+namespace ThingsLibrary.DataType
+{
+    public class TelemetryEntry
+    {
+        [JsonPropertyName("date")]
+        public DateTimeOffset Timestamp { get; set; } = DateTime.UtcNow;
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; } = string.Empty;
+
+        [JsonPropertyName("tags")]
+        public Dictionary<string, string> Attributes { get; set; } = [];
+
+        public TelemetryEntry()
+        {
+            //nothing
+        }
+
+        public TelemetryEntry(string type, DateTimeOffset timestamp)
+        {
+            this.Type = type;
+            this.Timestamp = timestamp;
+        }
+
+
+
+        /// <summary>
+        /// Generate the telemetry sentence
+        /// </summary>
+        /// <returns>Telemetry Sentence.. IE: $1724387849602|PA|r:1|s:143|p:PPE Mask|q:1|p:000*79</returns>
+        public override string ToString()
+        {
+            var sentence = new StringBuilder($"${this.Timestamp.ToUnixTimeMilliseconds()}|{this.Type}");
+            sentence.Append(string.Join(string.Empty, this.Attributes.Select(x => $"|{x.Key}:{x.Value}")));
+
+            sentence.AppendChecksum();
+
+            return sentence.ToString();
+        }
+
+        #region --- Static ---
+
+        public static TelemetryEntry Parse(string telemetrySentence) 
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
