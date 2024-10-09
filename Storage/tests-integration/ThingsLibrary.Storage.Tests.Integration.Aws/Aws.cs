48ef8180bd8e5a1c3597b7be9571bfc580d22e76
Mark Lanning <mark@lannings.org>
2024-10-09 18:47:07 -0500

    updated nuget and solution to include database

--- Storage/tests-integration/ThingsLibrary.Storage.Tests.Integration.Aws/Aws.cs
+++ Storage/tests-integration/ThingsLibrary.Storage.Tests.Integration.Aws/Aws.cs
@@ -1,4 +1,11 @@
-﻿using ThingsLibrary.Testing.Environment;
+﻿// ================================================================================
+// <copyright file="Aws.cs" company="Starlight Software Co">
+//    Copyright (c) Starlight Software Co. All rights reserved.
+//    Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+// </copyright>
+// ================================================================================
+
+using ThingsLibrary.Testing.Environment;
 
 namespace ThingsLibrary.Storage.Tests.Integration.Aws
 {
